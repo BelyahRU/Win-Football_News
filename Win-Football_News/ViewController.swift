b33b810f2bdc1afa74137e6b8c77dae9e2af57c0
Alexander <oBelyash@yandex.ru>
2024-09-26 20:42:21 +0300

    Initial Commit

---
+++ Win-Football_News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Win-Football_News
+//
+//  Created by Александр Андреев on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
